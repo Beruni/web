11efff26d30a8bf18ab19a93c9854af1884ec432
Jaideep Rana <jaideepk@thoughtworks.com>
2016-07-06 17:23:19 +0530

    Card #27 | [Bhawna/Jaideep] Fix Karma test

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -17,6 +17,15 @@ module.exports = {
 
       },
       {
+        test: /\.scss$/,
+        loader: 'null'
+
+      },
+      {
+        test: /\.jade$/,
+        loader: 'raw!jade-html'
+      },
+      {
         test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
         loader: 'null'
       },
