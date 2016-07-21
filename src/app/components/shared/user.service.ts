3db02e651e946f07d04c842693d08be56ddb3f12
yadavparmatma <pyadav.it@gmail.com>
2016-07-21 16:49:30 +0530

    +Vijay | login routes changed to authorize

--- src/app/components/shared/user.service.ts
+++ src/app/components/shared/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
     {
         console.log('avout to send to backend');
         let headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.post('http://localhost:3001/login',JSON.stringify(data),{headers:headers})
+        return this.http.post('http://localhost:3001/authorize',JSON.stringify(data),{headers:headers})
             .toPromise()
             .then(callback)
             .catch(this.handleError);
