f5a2f5ace6ad677e9a4be4931d6be163b1a22099
yadavparmatma <pyadav.it@gmail.com>
2016-07-28 13:35:20 +0530

    Revert "| TECH | Parmatma | User docker IP address for accessing userService"
    This reverts commit 7226d148d0a6f86683f10119757abfa26a1c459c.

--- src/app/components/shared/user.service.ts
+++ src/app/components/shared/user.service.ts
@@ -9,9 +9,9 @@ export class UserService {
     constructor(private http: Http) { }
 
     login(data:any): Promise<any> {
-        console.log('about to send to backend');
+        console.log('avout to send to backend');
         let headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.post('http://192.168.99.100:3001/authorize', JSON.stringify(data), {headers:headers})
+        return this.http.post('http://localhost:3001/authorize', JSON.stringify(data), {headers:headers})
             .toPromise();
     }
 }
