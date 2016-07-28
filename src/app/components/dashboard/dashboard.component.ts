6e0c5f0c13ae2030b2f94d62d0937a78ba4efb7a
Jaideep Rana <jaideepk@thoughtworks.com>
2016-07-28 17:37:39 +0530

    | #14 | Jaideep | Create sample UI for the menu at top for dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -5,7 +5,8 @@ import {DataFileComponent} from "./upload_data_file/upload_data_file.component";
 @Component({
     selector: 'dashboard',
     directives: [BoundaryFileComponent, DataFileComponent],
-    template: require('./dashboard.component.jade')
+    template: require('./dashboard.component.jade'),
+    styles: [require('./dashboard.component.scss')]
 })
 
 export class DashboardComponent {
