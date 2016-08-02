8787270f7c716bdce68d8438b2d16a0cbf85b6da
souvikd <souvikd@thoughtworks.com>
2016-08-02 13:05:55 +0530

    | #3 | Parmatma + Souvik | added progressbar for upload boundary file:

--- src/app/components/dashboard/upload_boundary_file/upload-boundary-file.component.ts
+++ src/app/components/dashboard/upload_boundary_file/upload-boundary-file.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {TagsInputComponent} from "../tags/tags.component";
-
+import {Response} from '@angular/http'
 @Component({
     selector: 'upload-boundary-file',
     template: require('./upload_boundary_file.component.jade'),
@@ -19,13 +19,35 @@ import {TagsInputComponent} from "../tags/tags.component";
 
 export class BoundaryFileComponent {
     public boundaryFile:{
-        tags:any[]
-    } = {tags: []};
+        tags:any[], title:string
+    } = {tags: [], title: ""};
 
     uploadSelectedFile() {
 
-        (<HTMLInputElement>document.getElementById("tags_field")).value = this.boundaryFile.tags.toString();
-        (<HTMLFormElement>document.getElementById("file_selector")).submit();
+        var fileName = (<HTMLInputElement>document.getElementById("boundaryFile")).files[0];
+
+        var data = new FormData();
+        data.append("boundaryFile", fileName);
+        data.append("title", this.boundaryFile.title);
+        data.append("tags", this.boundaryFile.tags.toString());
+
+        var request = new XMLHttpRequest();
+
+        request.onreadystatechange = function () {
+            console.log(request.response);
+        };
+        request.upload.addEventListener('progress', function (e:ProgressEvent) {
+            var progressbar = document.getElementById("progressbar");
+            var progressbarContainer = document.getElementById("progressbarContainer");
+            var uploadPercentage = Math.ceil(e.loaded / e.total * 100);
+            if (uploadPercentage) {
+                progressbarContainer.style.display = "block";
+                progressbar.style.width = uploadPercentage + '%';
+            }
+        }, false);
+        request.open("POST", "http://127.0.0.1:3000/upload/");
+        request.send(data);
+
     }
 
 }
