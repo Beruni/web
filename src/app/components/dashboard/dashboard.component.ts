c576e5983c16cdc203df24e159ebe6201473d0f3
tushar <garg.tushar.05@gmail.com>
2016-08-02 15:49:19 +0530

    #6 | tushar | deleted upload-data-component as it's no longer used

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,14 +2,13 @@ import {Component} from '@angular/core';
 import { SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES } from "ng-semantic";
 
 import {BoundaryFileComponent} from "./upload_boundary_file/upload-boundary-file.component";
-import {DataFileComponent} from "./upload_data_file/upload-data-file.component";
 import {UploadModalComponent} from "./upload_modal/upload-modal.component";
 
 @Component({
     selector: 'dashboard',
     template: require('./dashboard.component.jade'),
     styles: [require('./dashboard.component.scss')],
-    directives: [BoundaryFileComponent, DataFileComponent, UploadModalComponent, SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES]
+    directives: [BoundaryFileComponent, UploadModalComponent, SEMANTIC_COMPONENTS, SEMANTIC_DIRECTIVES]
 })
 
 export class DashboardComponent {
