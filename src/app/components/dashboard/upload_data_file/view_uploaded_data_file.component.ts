fedc982ec1e33073e520cf60c2b144e4f5658bb2
tushar <garg.tushar.05@gmail.com>
2016-08-03 14:52:25 +0530

    #6 | tushar | added upload data file button to data-file tab in dashboard

--- src/app/components/dashboard/upload_data_file/view_uploaded_data_file.component.ts
+++ src/app/components/dashboard/upload_data_file/view_uploaded_data_file.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {UploadModalComponent} from "../upload_modal/upload-modal.component";
 
 @Component({
   selector: 'view-uploaded-data-file',
-  template: require('./view_uploaded_data_file.component.jade')
+  template: require('./view_uploaded_data_file.component.jade'),
+  directives: [UploadModalComponent]
 })
 export class ViewDataFileComponent {
 }
