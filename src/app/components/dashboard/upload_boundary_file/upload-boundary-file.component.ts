b8242c005f1406fce24ecbcd0c91b277078c55cd
yadavparmatma <pyadav.it@gmail.com>
2016-08-03 11:47:02 +0530

    | #3 | Parmatm | seprated sccs file for boundary upload and upload percentage visualization

--- src/app/components/dashboard/upload_boundary_file/upload-boundary-file.component.ts
+++ src/app/components/dashboard/upload_boundary_file/upload-boundary-file.component.ts
@@ -1,18 +1,10 @@
-import {Component, Injectable} from '@angular/core';
+import {Component, Injectable} from "@angular/core";
 import {TagsInputComponent} from "../tags/tags.component";
-import {Response} from '@angular/http'
+
 @Component({
     selector: 'upload-boundary-file',
     template: require('./upload_boundary_file.component.jade'),
-    styles: [`
-        button{
-            margin-left: 10px;
-        }
-        form{
-            margin-left: 10px;
-            margin-bottom: 10px;
-        }
-    `],
+    styles: [require('./upload-boundary-file.component.scss')],
     directives: [TagsInputComponent]
 })
 
@@ -37,10 +29,12 @@ export class BoundaryFileComponent {
         request.upload.addEventListener('progress', function (e:ProgressEvent) {
             var progressbar = document.getElementById("progressbar");
             var progressbarContainer = document.getElementById("progressbarContainer");
+            var status = document.getElementById("status");
             var uploadPercentage = Math.ceil(e.loaded / e.total * 100);
             if (uploadPercentage) {
                 progressbarContainer.style.display = "block";
                 progressbar.style.width = uploadPercentage + '%';
+                status.innerHTML = uploadPercentage + '%';
             }
         }, false);
         request.open("POST", "http://127.0.0.1:3000/upload/");
