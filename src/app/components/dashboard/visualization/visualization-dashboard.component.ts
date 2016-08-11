658b662e007dc63898b547faad56cc7e6ce83304
souvikd <souvikd@thoughtworks.com>
2016-08-11 15:07:09 +0530

    Souvik + Vijay| Integrate Leaflet

--- src/app/components/dashboard/visualization/visualization-dashboard.component.ts
+++ src/app/components/dashboard/visualization/visualization-dashboard.component.ts
@@ -3,12 +3,7 @@ import {SemanticModalComponent} from "ng-semantic/ng-semantic";
 import * as L from "leaflet"
 @Component({
     selector: 'upload-visualization-modal',
-    template: `
-<head>
-    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css"/>
-</head>
-
-<div id="map" style="width: 80%;height: 80%"></div>`,
+    template: require('./visualization-dashboard.component.jade'),
     directives: [SemanticModalComponent]
 })
 
