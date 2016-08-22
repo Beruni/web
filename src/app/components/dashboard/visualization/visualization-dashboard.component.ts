101cd14eee38e6dddeb3f9a85054f262402a916d
yadavparmatma <pyadav.it@gmail.com>
2016-08-22 15:51:01 +0530

    | #6 | Parmatma + Souvik | boundary file visulization using leaflet

--- src/app/components/dashboard/visualization/visualization-dashboard.component.ts
+++ src/app/components/dashboard/visualization/visualization-dashboard.component.ts
@@ -11,21 +11,23 @@ import {Router} from "@angular/router";
 
 export class VisualizationDashBoardComponent {
 
-    constructor(private boundaryFileService: BoundaryFileService,private router: Router){}
+    constructor(private boundaryFileService:BoundaryFileService, private router:Router) {
+    }
 
     ngOnInit() {
         var query = this.router.url.split('/');
         var fileId = query[query.length - 1];
-        this.boundaryFileService.fetchBoundaryFileById(fileId, (data:JSON) => {
-            //TODO: populate this json to map
-            console.log(data);
+        this.boundaryFileService.fetchBoundaryFileById(fileId, (data:any) => {
+            this.loadMap(JSON.parse(data));
         })
-        this.loadMap();
     }
 
-    loadMap() {
-        var map = L.map('map').setView([42.35, -71.08], 13);
+    loadMap(data:JSON) {
+        var layer = L.geoJson(data);
+        var center = layer.getBounds().getCenter();
+        var map = L.map('map').setView(center).fitBounds(layer.getBounds());
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+        layer.addTo(map);
     }
 
 }
